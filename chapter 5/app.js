388773c55c4b70467dafae7c7d840e4906d29cce
hamayoon <155162842+hamayoonraza@users.noreply.github.com>
2024-02-28 02:59:45 +0500

    chapter 5. done

---
+++ chapter 5/app.js
@@ -0,0 +1,96 @@
+// Q 1//
+var nom1=25
+var nom2=7
+var modulus=nom1%nom2
+console.log(modulus)
+
+// Q 2//
+var nom1=15
+var nom2=4
+var addition=nom1+10
+console.log(addition)
+var subtraction=nom2-2
+console.log(subtraction)
+var multiplication=nom1*nom2
+console.log(multiplication)
+
+// Q 3//
+var nom1=12
+var nom2=3
+var Exponentiation=nom1**nom2
+console.log(Exponentiation)
+
+// Q 4//
+var e=10
+var f=3
+// var multiplication=f*2
+// console.log(multiplication)
+var addition=e+=f*2
+console.log(addition)
+// Q 5//
+var g=20
+var h=4
+var addition=g+5
+console.log(addition)
+var Division=g/h
+console.log(Division)
+// Q 6//
+var i=8
+var j=3
+var subtraction=i-2
+console.log(subtraction)
+var multiplication=subtraction*j
+console.log(multiplication)
+// Q 7//
+var k=18
+var l=5
+var subtraction=k-l
+console.log(subtraction)
+var addition=subtraction+2
+console.log(addition)
+// Q 8//
+var m=7
+var n=4
+var Exponentiation=m**n
+console.log(Exponentiation)
+// Q 9//
+var o=13
+var p=4
+var bodmas=o%(p+2)
+console.log(bodmas)
+// Q 10//
+var q=16
+var r=3
+var Division=q/2
+console.log(Division)
+var modulus=Division%r
+console.log(modulus)
+// Q 11//
+var s=25
+var t=3
+var subtraction=1-s
+console.log(subtraction)
+var Division=subtraction/8
+console.log(Division)
+// Q 12//
+var u=11
+var v=3
+var addition=2+v
+console.log(addition)
+var subtraction=u-addition
+console.log(subtraction)
+// Q 13//
+var w=22
+var x=5
+var addition=w+(x**2)
+console.log(addition)
+// Q 14//
+var y=19
+var z=4
+var modulus=y%(z+1)
+console.log(modulus)
+// Q 15//
+var a=10
+var b=3
+var bodmas=a*(b-2)
+console.log(bodmas)
