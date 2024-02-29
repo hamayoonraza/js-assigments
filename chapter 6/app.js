0367d61e74e0c3ca8d584829a1168d3260da3d33
hamayoon <155162842+hamayoonraza@users.noreply.github.com>
2024-03-01 01:53:44 +0500

    chapter 6.chapter 7 done

---
+++ chapter 6/app.js
@@ -0,0 +1,40 @@
+// Q no 1
+var a = 8
+a++
+console.log(a)
+// Q no 2
+var x=15
+--x
+console.log(x)
+// Q no 3
+var num=25
+num--
+console.log(num)
+// Q no 4
+var b=10
+++b
+console.log(b)
+// q no 5
+var z=5
+z++
+console.log(z)
+// q 6
+var d=12
+--d
+console.log(d)
+// q no 7
+var g=18
+g++
+console.log(g)
+// q no 8
+var score=30
+score--
+console.log(score)
+// q no 9
+var r=20
+++r
+console.log(r)
+// q no 10
+var f=8
+f--
+console.log(f)
---
+++ chapter 8/app.js
@@ -0,0 +1,57 @@
+// Concatenating text strings
+// q 1
+var firstName='john  '
+var lastName='deo'
+var fullName=firstName+lastName
+console.log(fullName)
+// q 2
+var cityName='New'
+var lcityName='Yor'
+var fullName2=cityName+lcityName
+console.log(fullName2)
+// q 3
+var fruit='aplle '
+var fruit1='pie'
+var fullName3=fruit+fruit1
+console.log(fullName3)
+// q 4
+var animal='black'
+var animal1='bird'
+var fullName4=animal+animal1
+console.log(fullName4)
+// q 5
+var word='javascript '
+var word2='is fun'
+var fullName5=word+word2
+console.log(fullName5)
+// q 6
+var cName='open '
+var cName1='source '
+var cName2='inteligence'
+var fullName6=cName+cName1+cName2
+console.log(fullName6)
+// q 7
+var bday='happy '
+var bday1='birthday '
+var bday2='to you'
+var fullName7=bday+bday1+bday2
+console.log(fullName7)
+// q 8
+var wish='good '
+var wish1='luck '
+var wish2='have '
+var wish3='fun'
+var fullName8=wish+wish1+wish2+wish3
+console.log(fullName8)
+// q 9
+var fName='web'
+var lName='design '
+var sName='develpment'
+var fullName9=fName+lName+sName
+console.log(fullName9)
+// q 10
+var ct='artificial '
+var ct1='neaural '
+var ct2='networks'
+var fullName10=ct+ct1+ct2
+console.log(fullName10)
